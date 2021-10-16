4b3836664a37a323c4d4b307dfadc7f981ce2aec
Ahmet Yasin <ahmetyasin0493@gmail.com>
2021-10-16 21:10:50 +0300

    first

---
+++ console-programlama/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace console_programlama{
+    class Program
+    {
+        static void Main(string[] args){
+            
+            Console.WriteLine("Adınızı giriniz: ");
+            string adi = Console.ReadLine();
+            Console.WriteLine("Soyadınızı giriniz: ");
+            string soyadi = Console.ReadLine();
+            Console.WriteLine("Selam " + adi + " " + soyadi);
+        }
+        
+    }
+
+}
+
