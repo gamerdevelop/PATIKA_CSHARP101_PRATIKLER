1ec97ddacb9e164582ed45584e801207f6d332a4
Ahmet Yasin <ahmetyasin0493@gmail.com>
2021-10-17 16:55:01 +0300

    first

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿
+   //Tamsayı sayısal degişkenler
+
+   byte b =10;
+   sbyte sb =-128;
+
+   short s = -32768;
+   ushort us = 65535;
+
+   int i = -2147483648;
+   uint ui = 4294967295;
+
+   long l = -9223372036854775808;
+   ulong ul = 4294967295;
+  
+  //Kesirli sayılar için
+
+  float f = 1.5f;
+  double d = 1.5d;
+  decimal m = 1.5m;
+
+  //Metinsel ifadeler için
+
+  string str = "İstediğim uzunlukta yazı atayabilirim";
+  
+  char c ='E'; //Sadece tek karakter atayabilirim. Tek tırnak arasına yazılır.
+
+  bool dy = 10 > 5; //True yada false degeri alır.10 sayısı 5 ten büyük olduğu için dy değişkenimiz true değerini aldı.
+
+  //Tarih ve Zaman 
+
+  DateTime ts = DateTime.Now;
+  Console.WriteLine(ts);
+
+  
