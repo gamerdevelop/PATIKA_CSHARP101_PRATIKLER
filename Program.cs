1186d9a33d9a06652663dd886d71ad68f4f3d367
Ahmet Yasin <ahmetyasin0493@gmail.com>
2021-10-18 18:07:13 +0300

    first

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿
+try{
+
+    Console.WriteLine("Bir sayı giriniz: ");
+    int sayi= Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Girmiş olduğunuz sayi: "+ sayi);
+
+}
+catch(Exception ex){
+     
+     Console.WriteLine("Hata: "+ ex.Message.ToString());
+}
+finally{
+    Console.WriteLine("Tamamlandı");
+}
