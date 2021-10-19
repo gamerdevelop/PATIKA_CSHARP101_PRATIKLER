2c82cedb9a1d25a76c99de445e5fe4fb6b88008d
Ahmet Yasin <ahmetyasin0493@gmail.com>
2021-10-19 16:48:03 +0300

    first

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿
+
+int time;
+
+Console.Write("Bir saat giriniz : ");
+time = DateTime.Now.Hour;
+
+if(time>14){
+    Console.WriteLine("Rojbaş");
+}
+else if(time<15) {
+    Console.WriteLine("Başe baş");
+}
+else{
+    Console.WriteLine("Şevbaş lo!");
+}
+
+
+
+
+    
+
