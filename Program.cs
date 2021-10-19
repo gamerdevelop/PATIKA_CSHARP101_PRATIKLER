1750d457a29e3646ae96232168d349258c156026
Ahmet Yasin <ahmetyasin0493@gmail.com>
2021-10-19 18:05:37 +0300

    second

--- Program.cs
+++ Program.cs
@@ -5,15 +5,24 @@ int time;
 Console.Write("Bir saat giriniz : ");
 time = DateTime.Now.Hour;
 
-if(time>14){
-    Console.WriteLine("Rojbaş");
+if(time>18){
+ Console.WriteLine("Rojbaş");
 }
-else if(time<15) {
+else if(time<18) {
     Console.WriteLine("Başe baş");
 }
 else{
     Console.WriteLine("Şevbaş lo!");
+
 }
+   string sonuc = time>=18 ?  "İyi günler!" : "İyi akşamlar!";
+   Console.WriteLine(sonuc);
+
+sonuc = time<19 && time>17 ? "Günaydın" : time>=18 ?  "İyi günler!" : "İyi akşamlar!";
+
+Console.WriteLine(sonuc);
+
+
 
 
 
