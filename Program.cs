de8aef5c541c9f0923ebdf0ce1457b7245f0469d
Ahmet Yasin <ahmetyasin0493@gmail.com>
2021-10-21 13:58:53 +0300

    first

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿Console.Write("Bir sayı giriniz => ");
+
+int sayi = Convert.ToInt32(Console.ReadLine());
+for (int i = 0; i <= sayi; i++)
+{
+    if (i % 2==1){
+        Console.Write(i + " ");
+    }    
+}
+Console.WriteLine();
+int ciftToplam=0,tekToplam=0;
+for (int i = 0; i <= 1000; i++)
+{
+    if(i%2==0){
+        ciftToplam+=i;
+    }
+    else{
+        tekToplam+=i;
+    }
+    if(i==5){
+        break;
+    }
+    
+
+}
+Console.WriteLine("Tek sayıların toplamı => "+ tekToplam);
+Console.WriteLine("Çift sayıların toplamı => "+ ciftToplam);
