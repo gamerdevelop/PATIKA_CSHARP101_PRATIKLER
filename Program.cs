399738fd7256ca4502ad4b60dcbfa016fcf84511
Ahmet Yasin <ahmetyasin0493@gmail.com>
2021-10-22 12:55:52 +0300

    genel duzenleme yapıldı ve tr karakterler alfabeye eklendi

--- Program.cs
+++ Program.cs
@@ -1,26 +1,60 @@
-﻿Console.WriteLine("Lütfen bir sayı giriniz =>");
+﻿Console.WriteLine("***** While *****");
+Console.Write("Lütfen bir sayı giriniz => ");
 double sayi = Convert.ToDouble(Console.ReadLine());
 double a=1,toplam=0;
-//1 den başlayarak konsoldan girilen sayıya kadar toplamını alıp ortalamasını alıp konsola yazdıran program.
+//1 den başlayarak konsoldan girilen sayıya kadar toplamının ortalamasını alıp konsola yazdıran program.
 while(a<=sayi)
 {
+    if(a<sayi)
+    Console.Write(a + "+");
+    else{
+        Console.Write(a);
+    }
     toplam+=a;
     a++;
+    
 }
-Console.WriteLine(toplam/sayi);
+Console.WriteLine("=" + toplam + " => " + toplam + "/" + sayi+"=" + toplam/sayi);
 
-//a' dan z' ye kadar bütün harfleri konsola yazdıran program.
+//a' dan z' ye kadar türkçe karakterler dahil harfleri konsola yazdıran program.
 char harf='a';
 while(harf<='z')
 {   
+    
     Console.Write(harf+" ");
+    switch (harf)
+    {
+        case 'c':
+            Console.Write("ç ");
+        break;
+        case 'g':
+            Console.Write("ğ ");
+        break;
+        case 'h':
+            Console.Write("ı ");
+        break;
+        case 'o':
+            Console.Write("ö ");
+        break;
+        case 's':
+            Console.Write("ş ");
+        break;
+        case 'u':
+            Console.Write("ü ");
+        break;
+
+        default:
+        break;
+    }
     harf++;
 }
 Console.WriteLine();
 Console.WriteLine("***** Foreach *****");
 
-string[] dizim= {"Ahmet", "Engin", "Yunus", "CIVAN","EKER","KAHRAMAN"};
+string[] dizim= {"Ahmet", "Engin", "Yunus"};
 foreach (string item in dizim)
 {
     Console.WriteLine(item);   
 }
+
+
